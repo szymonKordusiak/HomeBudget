2146b4daa8ad364451cdd9802d436b7ad6d947c6
szymonKordusiak <87189682+szymonKordusiak@users.noreply.github.com>
2022-04-02 18:21:56 +0200

    Prettier
    Kod sformatowany przez prettier

--- HomeBudget/ClientApp/src/app/app.server.module.ts
+++ HomeBudget/ClientApp/src/app/app.server.module.ts
@@ -3,8 +3,6 @@ import { ServerModule } from '@angular/platform-server';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 
-
-
 @NgModule({
     imports: [AppModule, ServerModule,  ],
     bootstrap: [AppComponent]
